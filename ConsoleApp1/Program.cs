9b7fc0f1f9c093aaa666e872ababa12c75c34326
Fernando <fgekdyszman@makingsense.com>
2021-01-19 15:40:39 -0300

    POC for integration a .NET app with azure speech-to-text

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var speechConfig = SpeechConfig.FromSubscription("8b78eb46c87c4209a6f24881da0aab0b", "southcentralus");
+            await FromMic(speechConfig);
+
+        }
+
+        async static Task FromMic(SpeechConfig speechConfig)
+        {
+            using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            using var recognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+            Console.WriteLine("Speak into your microphone.");
+            var result = await recognizer.RecognizeOnceAsync();
+            Console.WriteLine($"RECOGNIZED: Text={result.Text}");
+        }
+
+    }
+}
